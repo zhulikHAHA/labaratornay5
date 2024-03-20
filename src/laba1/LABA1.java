de56a9dfec710ddad435ade9d5633d6cb6bcc461
21645 <21645@1k129s4.dune.edu>
2024-03-20 08:19:29 +0200

    создать коммит 1 пункт 3

--- src/laba1/LABA1.java
+++ src/laba1/LABA1.java
@@ -15,7 +15,7 @@ public class LABA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("изменение 1");
     }
     
 }
