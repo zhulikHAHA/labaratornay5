59d34774542c1acd288e2a8b017c2d9f4275ac7b
21645 <21645@1k129s4.dune.edu>
2024-03-20 08:43:12 +0200

    11

--- src/laba1/LABA1.java
+++ src/laba1/LABA1.java
@@ -17,6 +17,9 @@ public class LABA1 {
     public static void main(String[] args) {
         System.out.println("первое изменение в коде");
         System.out.println("3 изменение в коде");
+        System.out.println("3 изменение в код1212");
+        System.out.println("ыварвыларлыолдвоаыаыва1212");
+        System.out.println("3 изменение в кодеыфвцфывцфывцфываыфвафц1212");
     }
     
 }
